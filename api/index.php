edbfafa2450cbf5f8e09972bad785d4f162d776c
HC <hclee@bcnetcom.com>
2022-07-28 17:55:35 +0900

    update the crt file location

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
 var_dump(openssl_get_cert_locations());
 $mysqli = mysqli_init();
-$mysqli->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
+$mysqli->ssl_set(NULL, NULL, "/etc/pki/tls/certs/ca-bundle.crt", NULL, NULL);
 $mysqli->real_connect($_ENV["HOST"], $_ENV["USERNAME"], $_ENV["PASSWORD"], $_ENV["DATABASE"]);
 $mysqli->close();
 phpinfo();
