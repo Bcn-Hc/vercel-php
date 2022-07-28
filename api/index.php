f8ad6d007e07d1db9c2d99ebb80b0bbd7934b3e9
HC <hclee@bcnetcom.com>
2022-07-28 17:49:27 +0900

    test db connection

--- api/index.php
+++ api/index.php
@@ -1,2 +1,7 @@
 <?php
+var_dump(openssl_get_cert_locations());
+$mysqli = mysqli_init();
+$mysqli->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
+$mysqli->real_connect($_ENV["HOST"], $_ENV["USERNAME"], $_ENV["PASSWORD"], $_ENV["DATABASE"]);
+$mysqli->close();
 phpinfo();
